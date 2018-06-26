279d793384b380742b8436629cf59dcb425e6da7
李冠宇 <lgy@lgy-mac.local>
2018-06-27 03:13:21 +0800

    first16

--- Test.php
+++ Test.php
@@ -3,10 +3,12 @@ require "logic.php";
 
 class Test extends SumTest
 {
+	# 0 1 2
+	# < = >
 	public function testTenPlusTen() {
-		$this->assertEquals($this->test(10,10),'=');
-		$this->assertEquals($this->test(11,10),'>');
-		$this->assertEquals($this->test(10,11),'<');
+		$this->assertEquals($this->test(10,10),0);
+		$this->assertEquals($this->test(11,10),2);
+		$this->assertEquals($this->test(10,11),0);
   	}
 }
 
--- logic.php
+++ logic.php
@@ -1,13 +1,15 @@
 <?php
 class SumTest
 {
+    # 0 1 2
+	# < = >
     public function test($num1, $num2) {
         if($num1>$num2){
-            return '<';
+            return 2;
         }elseif($num1<$num2){
-            return '<';
+            return 0;
         }else{
-            return '=';
+            return 1;
         }
     }
 }
