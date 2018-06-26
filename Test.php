3a0c504df7be70b4c5e9e8d53ddb675a4b0808b9
李冠宇 <lgy@lgy-mac.local>
2018-06-27 03:26:46 +0800

    first19

--- Test.php
+++ Test.php
@@ -8,7 +8,7 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testTenPlusTen() {
 		$compare = new CompareTest;
 		$this->assertEquals($compare->test(10,10),"=");
-		$this->assertEquals($compare->test(11,10),">");
+		$this->assertEquals($compare->test(11,10),"=");
 		$this->assertEquals($compare->test(10,11),"<");
   	}
 }
